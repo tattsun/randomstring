23ea82ad98d0d0b2b6b51206af98bd87a9fc3b0c
tattsun <t.t.mc1192.sf@gmail.com>
2020-07-24 01:43:57 +0900

    initial commit

---
+++ random.go
@@ -0,0 +1,26 @@
+package randomstring
+
+import "math/rand"
+
+type Generator interface {
+	Generate(length uint) string
+}
+
+var stdrunes = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func NewAlnumGenerator() *AlnumGenerator {
+	return &AlnumGenerator{}
+}
+
+type AlnumGenerator struct {
+}
+
+func (g *AlnumGenerator) Generate(length uint) string {
+	runes := make([]rune, length)
+
+	for i:=uint(0); i<length; i++ {
+		runes = append(runes, rune(stdrunes[rand.Intn(len(stdrunes)-1)]))
+	}
+
+	return string(runes)
+}
